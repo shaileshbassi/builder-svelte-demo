25034dc95386f4517c10f5b45f393de75303eaca
brijendrayadav <brijendra.yadav@nagarro.com>
2024-03-08 18:58:31 +0530

    search functionality implemented using algolia

---
+++ src/components/AlgoliaSearch/algoliaSearch.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  // import { onMount } from 'svelte';
+  import algoliasearch from "algoliasearch";
+
+  const searchApiKey: string = "66b05fd9e4eeb52216dd84647112dde1";
+  const searchAppId: string = "5660QXGNR5";
+  const indexName: string = "builder-svelte-page";
+
+  let searchResults: any[] = [];
+
+  let currentQuery: string = "";
+
+  const client = algoliasearch(searchAppId, searchApiKey);
+  const index = client.initIndex(indexName);
+
+  async function performSearch(query: string) {
+    if (query.trim() !== "") {
+      const resp = await index.search(query);
+      searchResults = resp.hits;
+      console.log(searchResults, "sss");
+    } else {
+      // If query is empty, clear the search results
+      searchResults = [];
+    }
+  }
+
+  async function handleSearch(event:any) {
+    currentQuery = event.target.value;
+    performSearch(currentQuery);
+  }
+</script>
+
+<!-- Search input -->
+<div class="flex justify-center mt-5">
+  <input
+    class="border-2 border-solid border-color-text-gray px-1 py-1 sm:w-1/2 md:w-1/3"
+    placeholder="Search product"
+    type="text"
+    on:input={(e) => handleSearch(e)}
+  />
+</div>
+
+<!-- Display search results -->
+<div class="flex justify-center mt-3">
+  {#each searchResults as result (result.objectID)}
+    <div
+      class="flex justify-center mx-3 w-1/4 border-[1px] border-solid border-color-text-gray bg-zinc-50 rounded hover:border-[2px] hover:border-black hover:bg-slate-100"
+    >
+      <div class="p-2">
+        <a href="{result.query[0].value}"><h5 class="text-lg font-bold">{result.data.title}</h5></a>
+      </div>
+    </div>
+  {/each}
+  </div>
--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  //@ts-nocheck
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
+  import Search from "../AlgoliaSearch/algoliaSearch.svelte";
   export let logoName: any;
   export let image: any;
   export let navLinks: any;
@@ -19,17 +21,17 @@
       currentLanguage = "en-US";
     }
 
-
     goto(newPath);
-
-
   }
-  
 </script>
 
-<header class="text-gray-600 body-font shadow-md z-10">
+<header class="flex flex-col text-gray-600 body-font shadow-md z-10 pb-8">
+  <div class="order-2 w-full">
+    <Search />
+  </div>
+  
   <div
-    class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+    class="container w-full mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
   >
     <div
       class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
@@ -40,20 +42,23 @@
       </a>
     </div>
     <nav
-      class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
-      data-sveltekit-reload
-    >
-      {#each navLinks as nav}
-        <a href={nav.url} class="mr-5 hover:text-gray-900">{nav.title}</a>
-      {/each}
-    </nav>
+    class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
+    data-sveltekit-reload
+  >
+    {#each navLinks as nav}
+      <a href={nav.url} class="mr-5 hover:text-gray-900">{nav.title}</a>
+    {/each}
+  </nav>
     <div class="langToggle">
-      <button class="px-2 py-1 border rounded-md flex items-center justify-center gap-1" on:click={changeLanguage}>
-       <img
+      <button
+        class="px-2 py-1 border rounded-md flex items-center justify-center gap-1"
+        on:click={changeLanguage}
+      >
+        <img
           src="https://www.svgrepo.com/show/116369/earth-globe-tool.svg"
           class="h-3 w-3"
           alt="language"
-        /> <span>{$page.url.pathname == '/en-FR'? 'US':'FR'}</span>
+        /> <span>{$page.url.pathname == "/en-FR" ? "US" : "FR"}</span>
       </button>
     </div>
   </div>
